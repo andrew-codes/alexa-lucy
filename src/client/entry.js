47a66aa7088671bd015f1444ece73bb7d78ee736
Andrew Smith <andrew@andrew.codes>
2016-12-29 11:16:17 -0500

    Dev env with hot loading
    - dev env does not run in a container
    	- too unreliable for it to pick up changes on the shared volumes to trigger webpack and hot loading
    - can optionally run a mongo service for dev purposes, so no longer need dev specific docker files
    - added webpack.config.js for production builds as well

--- src/client/entry.js
+++ src/client/entry.js
@@ -13,11 +13,9 @@ ReactDOM.render(
 
 if (module.hot) {
     module.hot.accept('./Root', () => {
-        console.log('hereae;jlrkaj;sfd')
         const NextApp = require('./Root').default;
         // If you use Webpack 2 in ES modules mode, you can
         // use <App /> here rather than require() a <NextApp />.
-        console.log(NextApp)
         ReactDOM.render(
             <AppContainer>
                 <NextApp />
